57c254b3915bce24852f68802c0d3b33b40892db
Kevin Mejia <kmejia@hnsc.net>
2018-09-04 16:13:27 -0600

    adding dependencies

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const os = require('os');
 const fs = require('fs');
 const mess = require('./message.js');
+const _ = require('lodash');
 
-let  cpu = os.cpus();
-let host = os.hostname();
+let x = {"nombre": "Arnold"}
+let y = {"apellido":"Perez"}
+let z = [
+    {nombre:"jaimme",apellido:"Medina", edad:26},
+    {nombre:"kevin",apellido:"Mejia",edad:25}
+]
 
-console.log(mess.friends);
-console.log(mess.welcome());
+let result = _.assign(x,y);
+console.log(result);
 
-
-
+let entrada = process.argv;
+console.log(entrada);
 
 
